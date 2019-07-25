dca778a7814ce32170d9727782822a01d5bfad47
Rea23 <36733245+Rea23@users.noreply.github.com>
2019-07-25 15:31:44 +0200

    First commit (#2)
    * second update
    
    * functionalities working
    
    * added dark/light mode switch

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,13 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
-import { Negative, SomeComp, Positive } from './ItemsMsg';
+
+import { SomeComp } from './ItemsMsg';
 import Item from './Item';
 
 function Main(props) {
-    // console.log(props.list)
-    // console.log(typeof(props.list))
-    // console.log(typeof(newP))
     return(
         <div>
             <SomeComp>Items:</SomeComp>
-            {/* {props.list.length > 0 ? props.list.map(item => <Item p={item} key={item.toString()} />) : <p>none</p>} */}
             {props.list.map(item => <Item p={item} key={item.toString()} />)}
-            
         </div>
     );
 }
