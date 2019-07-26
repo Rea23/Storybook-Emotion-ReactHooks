b03f656c4350d6bbe7933f48b763e51154424aa3
Stipan Hrzic <stipan.hrzic@gmail.com>
2019-07-26 16:44:10 +0200

    Bounce added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,17 @@
-import React, { Component } from 'react'
-
+/** @jsx jsx */
+import React, { Component } from "react";
+import { Bounce } from "./ItemsMsg";
+import { css, jsx } from "@emotion/core";
 export default class Header extends Component {
-    render() {
-        return (
-            <h1>
-                Header
-            </h1>
-        )
-    }
+  render() {
+    return (
+      <h1
+        css={css`
+          animation: ${Bounce} 2s ease-in-out infinite;
+        `}
+      >
+        Header
+      </h1>
+    );
+  }
 }
--- src/components/ItemsMsg.js
+++ src/components/ItemsMsg.js
@@ -1,27 +1,28 @@
-import styled from '@emotion/styled';
-import { keyframes } from 'emotion';
+/** @jsx jsx */
+import styled from "@emotion/styled";
+import { keyframes } from "@emotion/core";
 
 export const Negative = styled.div`
   background-color: red;
   font-weight: bold;
   color: white;
-  `
+`;
 export const Positive = styled.div`
   background-color: green;
   font-weight: bold;
-  `
+`;
 
 export const Button = styled.button`
   background-color: ${props => (props.bcg ? "yellow" : "green")};
   font-size: 16pt;
   margin: 12px 0;
-  `
+`;
 
 export const SomeComp = styled.div({
-    color: 'hotpink'
-  })
+  color: "hotpink"
+});
 
 export const Bounce = keyframes`
-from { top: 0; left: 0; }
-  to   { top: 100px; left: 100px; }
-`
+from, to{ transform: translateY(0); }
+50%{transform: translateY(-30px);}
+`;
